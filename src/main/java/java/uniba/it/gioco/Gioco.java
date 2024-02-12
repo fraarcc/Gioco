1feb9dc247dc6ed0fa5bf259086506a7b05389cd
fraarcc <f.ricci32@studenti.uniba.it>
2024-02-12 13:36:24 +0100

    Creata cartella src

---
+++ src/main/java/java/uniba/it/gioco/Gioco.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package java.uniba.it.gioco;
+
+/**
+ *
+ * @author 39379
+ */
+public class Gioco {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
