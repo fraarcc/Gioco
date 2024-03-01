458a85bb7685b086e5b169efeb9d2eeb4035e70d
fraarcc <f.ricci32@studenti.uniba.it>
2024-03-01 14:44:59 +0100

    Aggiunta delle classi al package tipi

---
+++ src/main/java/uniba/it/gioco/tipi/TipoComando.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uniba.it.gioco.tipi;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author 39379
+ */
+public enum TipoComando implements Serializable {
+    
+    NORD, SUD, EST, OVEST, INVENTARIO, ESCI, APRI, CERCA, CAMMINA, PARLA, INDOSSA, GUARDA, ACCENDI, LEGGI, SALVA, CARICA, LANCIATI, HELP
+    
+    
+}
--- src/main/java/uniba/it/gioco/tipi/prova.java
+++ src/main/java/uniba/it/gioco/tipi/prova.java
@@ -1,6 +1,6 @@
 package uniba.it.gioco.tipi;
 
-import java.util.LinkedHashSet;
+import java.util.HashSet;
 import java.util.Set;
 
 
@@ -18,18 +18,24 @@ public class prova {
     public static void main(String[] args) {
         
         
-       Oggetto o1 = new Oggetto(1,"prova1");
-       Oggetto o2 = new Oggetto(2,"prova2");
-       Oggetto o3 = new Oggetto(3,"prova3");
+      Oggetto o1 = new Oggetto(1,"prova1", "descrizione 1");
+      Oggetto o2 = new Oggetto(2,"prova2", "descrizione2");
+      Oggetto o3 = new Oggetto(3,"prova3","descrizione 3");
        
-       Set<Oggetto> insiemeogg = new LinkedHashSet<>();
+       Set<Oggetto> insiemeogg = new HashSet<>();
        insiemeogg.add(o1);
        insiemeogg.add(o2);
+       insiemeogg.add(o3);
+       Inventario inv = new Inventario(insiemeogg);
+       System.out.println(inv.toString());
+       inv.rimuovi(o3);
+       System.out.println(inv.toString());
        
        
-       Stanza s1= new Stanza(1,"stanza1","Descrizione1","Ispeziona1",true, insiemeogg);
        
-      System.out.println(s1.toString());
+       //Stanza s1= new Stanza(1,"stanza1","Descrizione1","Ispeziona1",true, insiemeogg);
+       
+      //System.out.println(s1.toString());
       
       
        
